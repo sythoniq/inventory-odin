9dca20692e46c01e6938952b282fae97cf737132
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-02-15 21:24:58 +0300

    started styling too late, running out of juice, a wasted sunday ic -_-

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ const index = require("./routes/index")
 const categories = require("./routes/categories")
 const items = require("./routes/items");
 
+const assetsPath = path.join(__dirname, 'publics');
+app.use(express.static(assetsPath));
 app.use(express.urlencoded({extended:true}));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 
 app.use("/", index);
 app.use("/categories", categories);
